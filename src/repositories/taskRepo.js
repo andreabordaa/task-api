a2742427abd3408a0419e647131a9bb173aab145
Andrea Borda <aborda2@uncc.edu>
2025-10-28 16:52:42 -0400

    Assignment 8

--- src/repositories/taskRepo.js
+++ src/repositories/taskRepo.js
@@ -4,6 +4,11 @@ export async function findAll() {
   return prisma.task.findMany();
 }
 
+export async function findTaskById(id) {
+  const task = await prisma.task.findUnique({ where: { id } });
+  return task;
+}
+
 // Create a new task
 export async function create(data) {
   return prisma.task.create({
